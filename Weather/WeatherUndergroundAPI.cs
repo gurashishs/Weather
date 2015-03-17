ff8abc877d68c8c2b4fb566936090fa12b1f95f9
Gurashish Singh <gurashishs@gmail.com>
2015-03-17 00:54:46 -0400

    Add comments. Fix namespace

--- Weather/WeatherUndergroundAPI.cs
+++ Weather/WeatherUndergroundAPI.cs
@@ -9,38 +9,54 @@ using System.Net;
 using System.Xml;
 using Newtonsoft.Json;
 
-namespace Autocomplete
+namespace Weather
 {
+    // WeatherUnderground API to interface with wunderground.com API
     class WeatherUndergroundAPI
     {
-        private string GEO_LOOKUP_STR = "http://api.wunderground.com/api/219620ed851cd61a/geolookup/q/autoip.json";
-        private string QUERY_STR = "http://autocomplete.wunderground.com/aq?c=US&h=0&format=JSON&query=";
-        private string FORECAST_STR = "http://api.wunderground.com/api/219620ed851cd61a/forecast/q/";
+        private const string API_KEY = "219620ed851cd61a";
+        private const string GEO_LOOKUP_STR = "http://api.wunderground.com/api/" + API_KEY + "/geolookup/q/autoip.json";
+        private const string FORECAST_STR = "http://api.wunderground.com/api/" + API_KEY + "/forecast/q/";
+
+        private const string QUERY_STR = "http://autocomplete.wunderground.com/aq?c=US&h=0&format=JSON&query=";
+        
         private HttpClient httpClient;
 
         public WeatherUndergroundAPI()
         {
-             this.httpClient = new HttpClient();
+            // Create new HTTP client
+            this.httpClient = new HttpClient();
         }
 
+        // Desc: Get forecast for the given city
+        // Input: City to get forecast for
+        // Output: Forecast for city. Returns async task. Needs to "await"ed
         public async Task<Forecast> getForecastForCity(City city)
         {
             ForecastResults forecastResults = await GetJsonObject<ForecastResults>(FORECAST_STR);
             return forecastResults.forecast;
         }
 
+        // Desc: Get City using the IP address of user using GeoLookup feature
+        // Output: City closest to user's IP address. Returns async task. Needs to "await"ed
         public async Task<City> getCityByGeoLookup()
         {
             GeoLookup geoLookup = await GetJsonObject<GeoLookup>(GEO_LOOKUP_STR);
             return LocationToCity(geoLookup.location);
         }
 
+        // Desc: Find the closest city name matches to given query using AutoCompler feature
+        // Input: String to query
+        // Output: List of cities with names closest to query. Returns async task. Needs to "await"ed
         public async Task<List<City>> getCitiesByNameQuery(string query)
         {
             CityResults cityResults = await this.GetJsonObject<CityResults>(QUERY_STR + query);
             return cityResults.RESULTS;
         }
 
+        // Desc: Convert Location object to City object.
+        // Input: Location returned by GeoLookup
+        // Output: City object that is a translation of input
         private City LocationToCity(Location location)
         {
             return new City()
@@ -58,13 +74,18 @@ namespace Autocomplete
             };
         }
 
-        // Very Helpful Resource = http://json2csharp.com/
+        // Desc: Gets JSON object given HTTP URL request. Very Helpful Resource = http://json2csharp.com/
+        // Input: HTTP request URL 
+        // Output: Returns the type of object requested. Returns async task. Needs to "await"ed
         private async Task<T> GetJsonObject<T>(string uri)
         {
             string result = await GetAsync(uri);
             return JsonConvert.DeserializeObject<T>(result);
         }
 
+        // Desc: Make asynchronous HTTP request
+        // Input: HTTP request URL 
+        // Output: Returns result string of HTTP request
         private async Task<string> GetAsync(string uri)
         {            
             return await this.httpClient.GetStringAsync(uri);
